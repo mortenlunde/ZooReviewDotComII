22e9607385a2da53e72818de08c98026be96a588
Morten Lunde <morten.lunde@live.no>
2024-11-12 00:17:52 +0100

    new migration for db on mac

---
+++ ZooReviewDotComII/Data/Migrations/20241111225730_macdb.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ZooReviewDotComII.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class macdb : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Reviews",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Reviews");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "UserId",
+                table: "Reviews",
+                type: "char(36)",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                collation: "ascii_general_ci");
+        }
+    }
+}
