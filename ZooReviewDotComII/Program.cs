e6b95aac233acfed6a806133e276895053bf5795
Morten Lunde <morten.lunde@live.no>
2024-11-14 16:54:06 +0100

    nicenice

--- ZooReviewDotComII/Program.cs
+++ ZooReviewDotComII/Program.cs
@@ -14,6 +14,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
 builder.Services.AddBlazorBootstrap();
+builder.Services.AddScoped<ZooService>();
 
 builder.Services.AddCascadingAuthenticationState();
 builder.Services.AddScoped<IdentityUserAccessor>();
---
+++ ZooReviewDotComII/Services/ZooService.cs
@@ -0,0 +1,20 @@
+using Microsoft.EntityFrameworkCore;
+using ZooReviewDotComII.Data;
+using ZooReviewDotComII.Models;
+
+namespace ZooReviewDotComII.Services;
+
+public class ZooService
+{
+    private readonly ApplicationDbContext _context;
+
+    public ZooService(ApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Zoo>> GetZoosAsync()
+    {
+        return await _context.Zoos.ToListAsync();
+    }
+}
